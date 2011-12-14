ad8bb7b301195ce7e7421efd274632399aa81b3d
Jake McCrary <jake@jakemccrary.com>
2011-12-13 20:23:47 -0600

    it works.
    Released version 0.0.2 from this code.

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
-(defproject autoexpect "1.0.0-SNAPSHOT"
-  :dependencies [[org.clojure/clojure "1.3.0"]
-                 [fresh "1.0.2"]
-                 [expectations "1.3.0"]])
+(defproject lein-autoexpect "0.0.3-SNAPSHOT"
+  :description "Automatically run expecations when a source file changes"
+  :url "https://github.com/jakemccrary/lein-autoexpect"
+  :developer "Jake McCrary"
+  :dependencies [[fresh "1.0.2"]])
--- src/leiningen/autoexpect.clj
+++ src/leiningen/autoexpect.clj
@@ -2,24 +2,30 @@
   (:use [leiningen.compile :only [eval-in-project]]))
 
 (defn autoexpect
+  "Autoruns expecations on source change"
   [project & args]
-  (eval-in-project
-   project
-   `(do
-      (let [stars# (apply str (repeat 15 "*"))
-            check# (fresh.core/freshener
-                    #(fresh.core/clj-files-in
-                      (clojure.java.io/file "test")
-                      (clojure.java.io/file "src")))]
-        (loop [_# nil]
-          (try
-            (let [report# (check#)]
-              (when-let [reloaded# (seq (:reloaded report#))]
-                (println stars# "Running tests" stars#)
-                (expectations/run-all-tests))
-              (Thread/sleep 500))
-            (catch Exception ex (println ex)))
-          (recur nil))))
-   nil nil `(do (require 'fresh.core
-                         'expectations
-                         'clojure.java.io))))
+  (let [test-path (get :test-path project "test")
+        source-path (get :source-path project "src")]
+    (eval-in-project
+     project
+     `(do
+        (reset! expectations/run-tests-on-shutdown false)
+        (let [top-stars#  (apply str (repeat 45 "*"))
+              side-stars# (apply str (repeat 15 "*"))
+              check# (fresh.core/freshener
+                      #(fresh.core/clj-files-in
+                        (clojure.java.io/file ~test-path)
+                        (clojure.java.io/file ~source-path)))]
+          (loop [_# nil]
+            (try
+              (let [report# (check#)]
+                (when-let [reloaded# (seq (:reloaded report#))]
+                  (println top-stars#)
+                  (println side-stars# "Running tests" side-stars#)
+                  (expectations/run-all-tests))
+                (Thread/sleep 500))
+              (catch Exception ex# (.printStackTrace ex#)))
+            (recur nil))))
+     nil nil `(do (require 'fresh.core
+                           'expectations
+                           'clojure.java.io)))))
