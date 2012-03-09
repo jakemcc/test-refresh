21c87f77b029649b1b1611bf7f31506135b88b36
Jake McCrary <jake@jakemccrary.com>
2012-03-09 09:43:26 -0600

    Initial cut at working with lein2.
    Still need to handle new keys in project.clj file (:test-paths,
        :source-paths)

--- src/leiningen/autoexpect.clj
+++ src/leiningen/autoexpect.clj
@@ -1,5 +1,25 @@
-(ns leiningen.autoexpect
-  (:use [leiningen.compile :only [eval-in-project]]))
+(ns leiningen.autoexpect)
+
+(defn eval-in-project
+  "Support eval-in-project in both Leiningen 1.x and 2.x."
+  [project form init]
+  (let [[eip two?] (or (try (require 'leiningen.core.eval)
+                            [(resolve 'leiningen.core.eval/eval-in-project)
+                             true]
+                            (catch java.io.FileNotFoundException _))
+                       (try (require 'leiningen.compile)
+                            [(resolve 'leiningen.compile/eval-in-project)]
+                            (catch java.io.FileNotFoundException _)))]
+    (if two?
+      (eip project form init)
+      (eip project form nil nil init))))
+
+(def dep ['fresh "1.0.2"])
+
+(defn- add-fresh-dep [project]
+  (if-let [conj-dependency (resolve 'leiningen.core.project/conj-dependency)]
+    (conj-dependency project dep)
+    (update-in project [:dependencies] conj dep)))
 
 (defn autoexpect
   "Autoruns expecations on source change"
@@ -7,7 +27,7 @@
   (let [test-path (get project :test-path "test")
         source-path (get project :source-path "src")]
     (eval-in-project
-     project
+     (add-fresh-dep project)
      `(do
         (reset! expectations/run-tests-on-shutdown false)
         (let [top-stars#  (apply str (repeat 45 "*"))
@@ -26,6 +46,6 @@
                 (Thread/sleep 500))
               (catch Exception ex# (.printStackTrace ex#)))
             (recur nil))))
-     nil nil `(do (require 'fresh.core
-                           'expectations
-                           'clojure.java.io)))))
+     `(require 'fresh.core
+               'expectations
+               'clojure.java.io))))
