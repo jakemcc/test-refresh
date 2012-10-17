4a0890f6df4e68c5bbc7970ef51eb521a8004d93
Jake McCrary <jake@jakemccrary.com>
2012-10-16 21:32:57 -0500

    bumping version number

--- project.clj
+++ project.clj
@@ -1,6 +1,5 @@
-(defproject lein-autoexpect "0.1.4"
+(defproject lein-autoexpect "0.2.0"
   :description "Automatically run expecations when a source file changes"
   :url "https://github.com/jakemccrary/lein-autoexpect"
   :developer "Jake McCrary"
-  :dependencies [[org.clojure/tools.namespace "0.2.0"]]
-  :lein-release {:deploy-via :clojars})
+  :dependencies [[org.clojure/tools.namespace "0.2.0"]])
