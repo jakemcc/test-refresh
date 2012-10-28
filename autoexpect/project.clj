02b66bf577cfb4a7519298a623115f9119ed7c20
Jake McCrary <jake@jakemccrary.com>
2012-10-28 15:48:55 -0500

    Bump version to 0.2.2-SNAPSHOT.

--- autoexpect/project.clj
+++ autoexpect/project.clj
@@ -1,4 +1,4 @@
-(defproject lein-autoexpect "0.2.1"
+(defproject lein-autoexpect "0.2.2-SNAPSHOT"
   :description "Automatically run expecations when a source file changes"
   :url "https://github.com/jakemcc/lein-autoexpect"
   :developer "Jake McCrary"
