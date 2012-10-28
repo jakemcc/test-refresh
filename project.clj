08df64754b3b97587adfc45a93151df0e9ad6b0c
Jake McCrary <jake@jakemccrary.com>
2012-10-28 14:37:52 -0500

    upgrade to org.clojure/tools.namespace 0.2.1

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "Automatically run expecations when a source file changes"
   :url "https://github.com/jakemcc/lein-autoexpect"
   :developer "Jake McCrary"
-  :dependencies [[org.clojure/tools.namespace "0.2.0"]
+  :dependencies [[org.clojure/tools.namespace "0.2.1"]
                  [jakemcc/clojure-gntp "0.1.1" :exclusions [org.clojure/clojure]]]
   :plugins [[lein-autoexpect "0.2.1-SNAPSHOT"]]
   :profiles {:dev {:dependencies [[expectations "1.4.10"]]}})
