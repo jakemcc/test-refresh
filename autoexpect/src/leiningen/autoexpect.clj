5393f6c4c4d4cb9e8002cbbfc83a4f27236e9908
Jake McCrary <jake@jakemccrary.com>
2012-12-29 15:55:34 -0600

    use more leinjacker

--- autoexpect/src/leiningen/autoexpect.clj
+++ autoexpect/src/leiningen/autoexpect.clj
@@ -1,23 +1,10 @@
 (ns leiningen.autoexpect
-  (:require [leinjacker.deps :as deps]))
-
-(defn- eval-in-project
-  "Support eval-in-project in both Leiningen 1.x and 2.x."
-  [project form init]
-  (let [[eip two?] (or (try (require 'leiningen.core.eval)
-                            [(resolve 'leiningen.core.eval/eval-in-project)
-                             true]
-                            (catch java.io.FileNotFoundException _))
-                       (try (require 'leiningen.compile)
-                            [(resolve 'leiningen.compile/eval-in-project)]
-                            (catch java.io.FileNotFoundException _)))]
-    (if two?
-      (eip project form init)
-      (eip project form nil nil init))))
+  (:require [leinjacker.deps :as deps]
+            [leinjacker.eval :as eval]))
 
 (defn- add-deps [project]
   (-> project
-      (deps/add-if-missing '[lein-autoexpect "0.2.4-SNAPSHOT"])
+      (deps/add-if-missing '[lein-autoexpect "0.2.5-SNAPSHOT"])
       (deps/add-if-missing '[org.clojure/tools.namespace "0.2.2"])))
 
 (defn ^{:help-arglists '([])} autoexpect
@@ -32,7 +19,7 @@ Runs expectations whenever code changes.
 Reports results to growl and STDOUT."
   [project & args]
   (let [should-growl (some #{:growl ":growl" "growl"} args)]
-    (eval-in-project
+    (eval/eval-in-project
      (add-deps project)
      `(autoexpect.runner/monitor-project ~should-growl)
      `(require 'autoexpect.runner))))
