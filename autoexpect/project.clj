3860274fbf7cffa838f851bc8538bd89c334558f
Jake McCrary <jake@jakemccrary.com>
2013-08-05 18:35:08 -0500

    adding lein-release as dep

--- autoexpect/project.clj
+++ autoexpect/project.clj
@@ -5,6 +5,7 @@
   :dependencies [[org.clojure/tools.namespace "0.2.4" :exclusions [org.clojure/clojure]]
                  [leinjacker "0.4.1" :exclusions [org.clojure/clojure]]
                  [jakemcc/clojure-gntp "0.1.1" :exclusions [org.clojure/clojure]]]
+  :plugins [[lein-release/lein-release "1.0.4"]]
   :profiles {:dev {:dependencies [[expectations "1.4.10"]]}}
   :lein-release {:scm :git
                  :deploy-via :clojars})
