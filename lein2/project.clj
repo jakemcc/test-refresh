557404b29baafcbb2415b9e04e69a6cbf24db9b6
Jake McCrary <jake@jakemccrary.com>
2014-02-02 19:39:37 -0600

    wait till you see my test results.

--- lein2/project.clj
+++ lein2/project.clj
@@ -4,4 +4,4 @@
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/tools.namespace "0.2.4"]]
   :plugins [[com.jakemccrary/lein-test-refresh ~(nth (read-string (slurp "../test-refresh/project.clj")) 2)]]
-  :test-refresh {:notify-command ["say"]})
+  :test-refresh {:notify-command ["say" "-v" "Whisper"]})
