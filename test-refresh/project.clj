09a40b592d90f13bcc927075c69e568e1ae09ddd
Jake McCrary <jake@jakemccrary.com>
2014-03-22 15:38:34 -0500

    Starting development on 0.3.11-SNAPSHOT

--- test-refresh/project.clj
+++ test-refresh/project.clj
@@ -1,4 +1,4 @@
-(defproject com.jakemccrary/lein-test-refresh "0.3.10"
+(defproject com.jakemccrary/lein-test-refresh "0.3.11-SNAPSHOT"
   :description "Automatically reload code and run clojure.test tests when files change"
   :url "https://github.com/jakemcc/lein-test-refresh"
   :developer "Jake McCrary"
