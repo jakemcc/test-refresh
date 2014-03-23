fe386495e68c338c22190b5d7aaa65b94b4b613f
Jake McCrary <jake@jakemccrary.com>
2014-03-22 20:36:06 -0500

    add some tests around when notifications should happen

---
+++ test-refresh/test/com/jakemccrary/test_refresh_test.clj
@@ -0,0 +1,14 @@
+(ns com.jakemccrary.test-refresh-test
+  (:require [com.jakemccrary.test-refresh :refer :all]
+            [clojure.test :refer :all]))
+
+(deftest test-should-notify?
+  (testing "if notify-on-success is true then all results should notify"
+    (is (should-notify? true {:status "Passed" :message "Testing"}))
+    (is (should-notify? true {:status "Failed" :message "Testing"}))
+    (is (should-notify? true {:status "Error" :message "Testing"})))
+
+  (testing "when notify-on-success false then Passed results do not notify"
+    (is (not (should-notify? false {:status "Passed" :message "Testing"})))
+    (is (should-notify? false {:status "Failed" :message "Testing"}))
+    (is (should-notify? false {:status "Error" :message "Testing"}))))
