11b6f6080d05252e16da2f30c0982d8aa15b1d20
Jake McCrary <jake@jakemccrary.com>
2014-05-24 19:00:09 -0500

    stop overriding the default tests selector

--- lein2/project.clj
+++ lein2/project.clj
@@ -4,8 +4,7 @@
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/tools.namespace "0.2.4"]]
   :plugins [[com.jakemccrary/lein-test-refresh ~(nth (read-string (slurp "../test-refresh/project.clj")) 2)]]
-  :test-selectors {:default (constantly nil)
-                   :integration :integration
+  :test-selectors {:integration :integration
                    :unit (complement :integration)
                    :fast (complement :slow)}
   :test-refresh {:notify-command ["say" "-v" "Agnes"]
