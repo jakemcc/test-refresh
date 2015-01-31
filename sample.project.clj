e97e1a452724df7a6a831be0fc39b123a9406809
Jake McCrary <jake@jakemccrary.com>
2015-01-31 12:21:44 -0600

    0.6.0 is about to be released

--- sample.project.clj
+++ sample.project.clj
@@ -1,6 +1,6 @@
 (defproject sample "1.2.3"
   :dependencies [[org.clojure/clojure "1.6.0"]]
-  :profiles {:dev {:plugins [[com.jakemccrary/lein-test-refresh "0.5.5"]]}}
+  :profiles {:dev {:plugins [[com.jakemccrary/lein-test-refresh "0.6.0"]]}}
 
   :test-refresh {;; Specifies a command to run on test
                  ;; failure/success. Short message is passed as the
