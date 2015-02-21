4499b3c12b31b549b5673bdc40a191d1186a0eed
Jake McCrary <jake@jakemccrary.com>
2015-02-21 13:23:57 -0600

    no longer depend on clojure in released versions

--- test-refresh/project.clj
+++ test-refresh/project.clj
@@ -5,13 +5,13 @@
   :min-lein-version "2.4"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/tools.namespace "0.2.8" :exclusions [org.clojure/clojure]]
+  :dependencies [[org.clojure/tools.namespace "0.2.8" :exclusions [org.clojure/clojure]]
                  [leinjacker "0.4.2" :exclusions [org.clojure/clojure]]
                  [jakemcc/clojure-gntp "0.1.1" :exclusions [org.clojure/clojure]]]
   :deploy-repositories [["snapshots" {:url "https://clojars.org/repo"
                                       :username :gpg :password :gpg}]
                         ["releases" {:url "https://clojars.org/repo"
                                      :username :gpg :password :gpg}]]
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.6.0"]]}}
   :scm {:name "git"
         :url "git@github.com:jakemcc/lein-test-refresh.git"})
