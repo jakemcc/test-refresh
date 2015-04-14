ec25ab7aa5502b8bd3f54d39d6000a5d04210e79
Jake McCrary <jake@jakemccrary.com>
2015-04-14 09:00:21 -0500

    test.check example

---
+++ lein2/test/lein2/test_check_test.clj
@@ -0,0 +1,17 @@
+(ns lein2.test-check-test
+  (:require [clojure.test.check :as tc]
+            [clojure.test.check.clojure-test :refer [defspec]]
+            [clojure.test.check.generators :as gen]
+            [clojure.test.check.properties :as prop]
+            [clojure.test :refer :all]))
+
+(defspec double-sort-works
+  100
+  (prop/for-all [v (gen/vector gen/int)]
+                (= (sort v) (sort (sort v)))))
+
+
+(defspec not-42
+  100
+  (prop/for-all [v (gen/vector gen/int)]
+    (not (some #{42} v))))
