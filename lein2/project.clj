3a4cea848581f9ac0db5973e33b357d7092d88c9
Jake McCrary <jake@jakemccrary.com>
2016-05-14 08:21:32 -0500

    set sample to 1.5

--- lein2/project.clj
+++ lein2/project.clj
@@ -1,7 +1,7 @@
 (defproject lein2 "0.1.0-SNAPSHOT"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/test.check "0.7.0"]]
   :plugins [[com.jakemccrary/lein-test-refresh #=(eval (nth (read-string (slurp "../test-refresh/project.clj")) 2))]]
   :test-selectors {:integration :integration
