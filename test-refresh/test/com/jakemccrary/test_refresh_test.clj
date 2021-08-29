6e046a3418d730a1863fcdf063e60f22e5a6f218
Jake McCrary <jake@jakemccrary.com>
2021-08-29 17:32:46 -0500

    clean up test namespace

--- test-refresh/test/com/jakemccrary/test_refresh_test.clj
+++ test-refresh/test/com/jakemccrary/test_refresh_test.clj
@@ -1,15 +1,15 @@
 (ns com.jakemccrary.test-refresh-test
-  (:require [com.jakemccrary.test-refresh :refer :all]
-            [clojure.test :refer :all]))
+  (:require [com.jakemccrary.test-refresh :as tr]
+            [clojure.test :refer [deftest is testing]]))
 
 (deftest test-should-notify?
   (testing "if notify-on-success is true then all results should notify"
-    (is (should-notify? true {:status "Passed" :message "Testing"}))
-    (is (should-notify? true {:status "Failed" :message "Testing"}))
-    (is (should-notify? true {:status "Error" :message "Testing"})))
+    (is (tr/should-notify? true {:status "Passed" :message "Testing"}))
+    (is (tr/should-notify? true {:status "Failed" :message "Testing"}))
+    (is (tr/should-notify? true {:status "Error" :message "Testing"})))
 
   (testing "when notify-on-success false then Passed results do not notify"
-    (is (not (should-notify? false {:status "Passed" :message "Testing"})))
-    (is (should-notify? false {:status "Failed" :message "Testing"}))
-    (is (should-notify? false {:status "Error" :message "Testing"}))))
+    (is (not (tr/should-notify? false {:status "Passed" :message "Testing"})))
+    (is (tr/should-notify? false {:status "Failed" :message "Testing"}))
+    (is (tr/should-notify? false {:status "Error" :message "Testing"}))))
 
